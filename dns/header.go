1ffd48e842c0b0daaea6a2bc57972fb8bb9e038d
Lucas Melin <lucas.melin@gmail.com>
2023-06-20 20:12:44 -0400

    Add docstrings

--- dns/header.go
+++ dns/header.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 )
 
+// Header implements a DNS message header.
+// See: https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.1
 type Header struct {
 	// ID is a 16 bit identifier assigned by the program that generated the query.
 	// This identifier is copied to the corresponding reply,
@@ -26,22 +28,37 @@ type Header struct {
 	//    it directs the name server to pursue the query recursively. Recursive query support is optional.
 	//  - RA - a one bit field that is set or cleared in a response, and denotes whether recursive query support is
 	//    available in the name server.
-	//  - Z - a one bit field that is reserved for future use. Must be zero in all queries and responses.
-	Flags          uint16
-	NumQuestions   uint16
-	NumAnswers     uint16
+	//  - Z - a three bit field that is reserved for future use. Must be zero in all queries and responses.
+	//  - RCODE - a four bit field that is set as part of responses. The values are no error condition (0),
+	//    a format error (1) meaning the name server was unable to interpret the query, a server failure (2) meaning
+	//    the name server was unable to process the query due to a problem with the name server, a name error (3) meaning
+	//    that the domain name referenced in the query does not exist, not implemented (4) meaning the name server does
+	//    not support the requested kind of query, refused (5) meaning the name server refuses to perform the specified
+	//    operation for policy reasons, reserved for future use (6-15).
+	Flags uint16
+	// NumQuestions represents QDCOUNT, an unsigned 16-bit integer specifying the
+	// number of entries in the question section.
+	NumQuestions uint16
+	// NumAnswers represents ANCOUNT, an unsigned 16-bit integer specifying the
+	// number of resource records in the answer section.
+	NumAnswers uint16
+	// NumAuthorities represents NSCOUNT, an unsigned 16-bit integer specifying the
+	// number of name server resource records in the authority records section.
 	NumAuthorities uint16
+	// NumAdditionals represents ARCOUNT, an unsigned 16-bit integer specifying the
+	// number of resource records in the additional records section.
 	NumAdditionals uint16
 }
 
-func ParseHeader(header *bytes.Reader) Header {
+// ParseHeader parses a given bytes reader into a Header.
+func ParseHeader(reader *bytes.Reader) Header {
 	h := Header{}
-	binary.Read(header, binary.BigEndian, &h.ID)
-	binary.Read(header, binary.BigEndian, &h.Flags)
-	binary.Read(header, binary.BigEndian, &h.NumQuestions)
-	binary.Read(header, binary.BigEndian, &h.NumAnswers)
-	binary.Read(header, binary.BigEndian, &h.NumAuthorities)
-	binary.Read(header, binary.BigEndian, &h.NumAdditionals)
+	binary.Read(reader, binary.BigEndian, &h.ID)
+	binary.Read(reader, binary.BigEndian, &h.Flags)
+	binary.Read(reader, binary.BigEndian, &h.NumQuestions)
+	binary.Read(reader, binary.BigEndian, &h.NumAnswers)
+	binary.Read(reader, binary.BigEndian, &h.NumAuthorities)
+	binary.Read(reader, binary.BigEndian, &h.NumAdditionals)
 
 	return h
 }
